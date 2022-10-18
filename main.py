e5cfbec7325c8a92b3518de3cfba7b0d50877493
Cami1212 <111406620+Cami1212@users.noreply.github.com>
2022-10-17 19:21:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+
 with open("calles_de_medellin_con_acoso.csv") as archivo:
    cadenaDeCaracteres = archivo.read( )
 
-print(cadenaDeCaracteres)
 
 
 with open("calles_de_medellin_con_acoso.csv") as archivo:
    listaDeLineasDondeCadaLineaEsUnaCadena = archivo.readlines( )
 
-print (listaDeLineasDondeCadaLineaEsUnaCadena)
+
+with open("poligono_de_medellin.csv") as archivo:
+   cadenaDeCaracteres = archivo.read( )
+
+
+with open("poligono_de_medellin.csv") as archivo:
+   listaDeLineasDondeCadaLineaEsUnaCadena = archivo.readlines( )
+
