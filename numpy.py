ac60c65437c072bceb1ad32e652bd179b990019c
Cami1212 <111406620+Cami1212@users.noreply.github.com>
2022-10-17 19:14:40 -0500

    Update numpy.py

--- numpy.py
+++ numpy.py
@@ -2,16 +2,7 @@ import numpy as np
 my_data = np.getfromtxt( 'poligono_de_medellin.csv' , delimiter=' ; ' )
 my_data = np.getfromtxt( 'calles_de_medellin_con_acoso.csv' , delimiter=' ; ' )
 
-with open("calles_de_medellin_con_acoso.csv") as archivo:
-   cadenaDeCaracteres = archivo.read( )
 
-print(cadenaDeCaracteres)
-
-
-with open("calles_de_medellin_con_acoso.csv") as archivo:
-   listaDeLineasDondeCadaLineaEsUnaCadena = archivo.readlines( )
-
-print (listaDeLineasDondeCadaLineaEsUnaCadena)
 
  
  
