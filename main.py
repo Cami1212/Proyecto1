aaea6f738180545e33b3fcda45f203786090ec9d
Cami1212 <111406620+Cami1212@users.noreply.github.com>
2022-10-17 19:14:59 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+with open("calles_de_medellin_con_acoso.csv") as archivo:
+   cadenaDeCaracteres = archivo.read( )
+
+print(cadenaDeCaracteres)
+
+
+with open("calles_de_medellin_con_acoso.csv") as archivo:
+   listaDeLineasDondeCadaLineaEsUnaCadena = archivo.readlines( )
+
+print (listaDeLineasDondeCadaLineaEsUnaCadena)
